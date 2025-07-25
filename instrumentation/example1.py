0aaeff4908bb3f4b21123dbb36e32e707baef72a
yooodleee <accia25@naver.com>
2025-07-25 14:50:44 +0900

    add count exceptions

--- instrumentation/example1.py
+++ instrumentation/example1.py
@@ -1,11 +1,18 @@
 import http.server
+import random
 from prometheus_client import start_http_server, Counter
 
 REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
+EXCEPTIONS = Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
+    # @EXCEPTIONS.count_exceptions()
     def do_GET(self):
         REQUESTS.inc()
+        with EXCEPTIONS.count_exceptions():
+            if random.random() < 0.2:
+                raise Exception
+            
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
