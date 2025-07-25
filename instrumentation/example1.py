0d0ac481e04ff88df41e8ff0d5377c780c9b8060
yooodleee <accia25@naver.com>
2025-07-25 14:40:38 +0900

    add counter

--- instrumentation/example1.py
+++ instrumentation/example1.py
@@ -1,8 +1,11 @@
 import http.server
-from prometheus_client import start_http_server
+from prometheus_client import start_http_server, Counter
+
+REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        REQUESTS.inc()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
